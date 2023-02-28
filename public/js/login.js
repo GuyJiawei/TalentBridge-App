cfd9078b19422e01ab59bc23964351fa9cc40069
Guy J Anderson <88031354+GuyJiawei@users.noreply.github.com>
2023-02-28 21:12:01 +1100

    completed login and signuphandler logic

--- public/js/login.js
+++ public/js/login.js
@@ -1,14 +1,38 @@
-const loginForm = async (event) => {
+const loginFormHandler = async (event) => {
     event.preventDefault();
-
-    const email = document.querySelector("#inputEmail").value.trim();
-    const password = document.querySelector("#inputPassword").value.trim();
-
+  
+    const email = document.querySelector('#inputEmail').value.trim();
+    const password = document.querySelector('#inputPassword').value.trim();
+  
     if (email && password) {
-        const response = await fetch("/api/user/login"), {
-            method: "POST",
-            body: JSON.stringify({ email, password }),
-            headers
-        }
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Invalid Login Details');
+      }
     }
-}
+  };
+  
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    if (event) {
+      const response = await fetch('/signup', {
+        method: 'GET'
+      });
+  
+      if (response.ok) {
+        document.location.replace('/signup');
+      } else {
+        console.error(err);
+        alert(response.statusText);
+      }
+    }
+};
