18aebbc899de4f9992a5c1c1dad50806d783d9ae
Guy J Anderson <88031354+GuyJiawei@users.noreply.github.com>
2023-03-02 19:38:47 +1100

    added homeroutes to render handelbars

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+
+router.get('/', async (req, res) => {
+  try {
+    res.render('homepage');
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/signup', async (req, res) => {
+  try {
+    res.render('signup');
+  } catch (err) {
+    res.statusMessage(500).json(err);
+  }
+});
+
+module.exports = router;
