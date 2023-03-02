3a006a9f442b34a318cff86c44a048498c754ecc
Majid-pkz <majid.computing@gmail.com>
2023-03-02 21:34:19 +1100

    add new api to userRoutes- job applied by a user

--- controllers/api/user-jobRoutes.js
+++ controllers/api/user-jobRoutes.js
@@ -14,46 +14,6 @@ router.get('/', async (req, res) => {
     }
 });
 
-// get all jobs applied by a user
-router.get('/uid/:user_id', async (req, res) => {
-    try {
-        const applicant = await UserJobs.findAll(
-            {
-                where: {
-                  user_id: req.params.user_id
-                }
-              }
-        );
-
-        res.status(200).json(applicant);
-    } catch (err) {
-        console.log(err)
-        res.status(500).json(err);
-        
-    }
-});
-
-// get all users  applied for a job
-
-router.get('/jid/:job_id', async (req, res) => {
-     
-    try {
-        const applicant = await UserJobs.findAll(
-            {
-                where: {
-                  jobs_id: req.params.job_id
-                }
-              }
-        );    
-        res.status(200).json(applicant);
-    } catch (err) {
-        console.log(err)
-        res.status(500).json(err);
-    }
-});
-
 // post to user-job once a user applied  -- to be done
-
-
 
 module.exports = router;
