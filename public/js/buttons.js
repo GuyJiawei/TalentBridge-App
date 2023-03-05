bc416bf05bdc390570b82292be1a10c2bc10d797
Katherine Echaiz <flor_echaiz@hotmail.com>
2023-03-05 21:04:39 +1100

    added button.js to make active tab highligther not working just jet

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -1,17 +1,4 @@
-// // Get the container element
-// var btnContainer = document.getElementById("nav-links");
 
-// // Get all buttons with class="btn" inside the container
-// var btns = btnContainer.getElementsByClassName("btnN");
-
-// // Loop through the buttons and add the active class to the current/clicked button
-// for (var i = 0; i < btns.length; i++) {
-//     btns[i].addEventListener("click", function () {
-//         var current = document.getElementsByClassName("active");
-//         current[0].remove('active');
-//         // this.className += " active";
-//     });
-// }
 
 function toggle_active(){
     console.log('Hello');
