227e978b82a09a3df285eaeb4e80b316cccb56ed
Katherine Echaiz <flor_echaiz@hotmail.com>
2023-03-07 16:50:19 +1100

    add code for buttons js and index.js for active buttons css

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -1,15 +1,20 @@
 
-
-function toggle_active(event){
-    console.log(event);
-    var elements= document.getElementsByTagName('a');
-    for(var i=0; i<=elements.length; i++) {
-        if (!(elements[i].getElementByClassName("active"))){
-            elements[i].classList.remove("active");
-        }
-        else{
-            elements[i].replaceClass(" ", "active");
-        }
-    }
-
+// function toggle_active(event){
+//     console.log(event);
+//     var elements= document.getElementsByTagName('a');
+//     for(var i=0; i<=elements.length; i++) {
+//         if (!(elements[i].getElementByClassName("active"))){
+//             elements[i].classList.remove("active");
+//         }
+//         else{
+//             elements[i].replaceClass(" ", "active");
+//         }
+//     }}
+const a = document.getElementsByTagName('a')
+for (let i = 0; i < a.length; i++) {
+    a[i].classList.remove(
+        'active');
+    console.log(a[i].innerText);
+    if (a[i].innerText.trim() == 'Sign up')
+        a[i].classList.add('active')
 }
