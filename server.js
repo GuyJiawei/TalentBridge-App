993d3938b5b9db36a23567c64cf030d8fa3e5525
sudarshans471-pqqgaF <sudarshans471@gmail.com>
2023-03-08 20:45:23 +1100

    feauture-multer

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const moment = require('moment');
-
+const multer = require('multer');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
